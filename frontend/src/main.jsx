17f200c2b5977fb34d7110ff79710f4b717ffb99
Pulkit6377 <pulkitgarg6377@gmail.com>
2025-10-21 12:14:42 +0530

    Moved backend files into backend folder and added frontend folder

---
+++ frontend/src/main.jsx
@@ -0,0 +1,13 @@
+import './index.css'
+import App from './App.jsx'
+import ReactDOM from 'react-dom/client.js'
+import {BrowseRouter} from 'react-router-dom'
+
+
+ReactDOM.createRoot(document.getElementById('root')).render(
+  <BrowseRouter>
+      <App />
+  </BrowseRouter>
+
+
+)
